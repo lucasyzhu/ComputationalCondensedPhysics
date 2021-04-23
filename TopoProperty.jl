c404c2eb6df67d427f16a1e3090db6dbe388be44
Lucas Hu <yizhihu1996@gmail.com>
2021-04-23 23:11:48 +0800

    v0.0.0

--- TopoProperty.jl
+++ TopoProperty.jl
@@ -1,41 +1,47 @@
 # This script computes Chern number and Berry Curvature of tight-binding model 
-# using an effective method from ...
+# using Fukui-Hatsugai-Suzuki method
 # authored by Lucas Hu
 # 2020-04-21 v0.0.0 The case of Haldane model is TODO 
 
 using LinearAlgebra, PyPlot
 
-function testChernInsulator()
+function testTopoChernInsulator()
     n = 100
     dk = 2*pi/n
+    m = 0
     chernNum::ComplexF64 = 0
-    BerryCurvature = Vector()
+    BerryCurvature1 = Vector()
+    BerryCurvature2 = Vector()
+    AnalyticBC(x, y) = -0.5*(cos(x)*((2 + m)*cos(y) - 1) - cos(y))/
+                       ((sin(x)^2 + sin(y)^2) + ((2 + m) - cos(x) -cos(y))^2)^(3/2)
     for kx in -pi:dk:(pi - dk)
         for ky in -pi:dk:(pi - dk)
+            append!(BerryCurvature1, AnalyticBC(kx, ky))
             H = findHamiltonianChern(kx, ky)
             eigenvectors = eigvecs(H)
-            vector = eigenvectors[:, 2]
+            vector = eigenvectors[:, 1]
             Hdkx = findHamiltonianChern(kx + dk, ky)
             eigenvectors = eigvecs(Hdkx)
-            vectordkx = eigenvectors[:, 2]
+            vectordkx = eigenvectors[:, 1]
             Hdky = findHamiltonianChern(kx, ky + dk)
             eigenvectors = eigvecs(Hdky)
-            vectordky = eigenvectors[:, 2]
+            vectordky = eigenvectors[:, 1]
             Hdkxdky = findHamiltonianChern(kx + dk, ky + dk)
             eigenvectors = eigvecs(Hdkxdky)
-            vectordkxdky = eigenvectors[:, 2]
+            vectordkxdky = eigenvectors[:, 1]
             Ux = vector'*vectordkx/abs(vector'*vectordkx) # ?? dot(conj(vector),vectordkx)
             Uy = vector'*vectordky/abs(vector'*vectordky)
             Uxy = vectordky'*vectordkxdky/abs(vectordky'*vectordkxdky)
             Uyx = vectordkx'*vectordkxdky/abs(vectordkx'*vectordkxdky)
             Fxy = log(Ux*Uyx*(1/Uxy)*(1/Uy))
-            append!(BerryCurvature, real(Fxy/1im))
+            append!(BerryCurvature2, real(Fxy/1im))
             chernNum += Fxy
         end
     end
     chernNum /= (2*pi*1im)
-    BerryCurvatureZ = reshape(BerryCurvature, (n - 1, n - 1))
-    plotFigure2D(BerryCurvatureZ, dk)
+    AnalyticBerryCur = reshape(BerryCurvature1, (n - 1, n - 1))
+    BerryCurvatureZ = reshape(BerryCurvature2, (n - 1, n - 1))
+    plotFigure2D(AnalyticBerryCur, BerryCurvatureZ, dk)
     println("Chern number of the model is ", chernNum)
     
 end
@@ -58,7 +64,7 @@ end
 function findHamiltonianChern(kx, ky)
     t = 1
     B = 1
-    M = -1
+    M = 0
     hx = t*sin(kx)
     hy = t*sin(ky)
     hz = B*(2 + M - cos(kx) -cos(ky))
@@ -94,15 +100,24 @@ function constructPauli()
     return sigmax, sigmay, sigmaz
 end
 
-function plotFigure2D(Z, dk)
+function plotFigure2D(Z1, Z2, dk)
     x = -pi:dk:(pi - dk)
     y = -pi:dk:(pi - dk)
-    fig = pcolormesh(x, y, Z)
-    colorbar(fig)
+    fig = figure(figsize = (13, 4.8))
+    subplot(121)
+    ax1 = pcolormesh(x, y, Z1)
+    colorbar(ax1)
     xlabel("kx")
-    ylabel("ky")
-    title("BerryCurvature")
+    ylabel("ky") 
+    title("Exact")
+    subplot(122)
+    ax2 = pcolormesh(x, y, Z2)
+    colorbar(ax2)
+    xlabel("kx")
+    ylabel("ky") 
+    title("Numeric")
     savefig("BerryCurvature.png", bbox_inches = "tight", dpi = 300)
 end
 
-@time testChernInsulator()
+
+@time testTopoChernInsulator()
